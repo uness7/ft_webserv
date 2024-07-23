8edd4b6904da9c6f9d94a8e97755c09c4ae4000b
Omar <omar.tourabi7@gmail.com>
2024-07-23 17:00:19 +0000

    ADD: epoll impl

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -5,19 +5,20 @@
 #include "TCPSocket.hpp"
 #include <arpa/inet.h>
 #include <iostream>
-#include <map>
 #include <netinet/in.h>
-#include <poll.h>
+#include <sys/epoll.h>
 #include <sstream>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <vector>
+#include <map>
 
 class Server {
 private:
   std::vector<TCPSocket *> _sockets;  // Vector to store pointers to TCPSocket instances
   std::map<unsigned short, Client *> _clients;  // Map to store connected clients
+  int _epoll_fd;
 
   void startToListenClients();
   void acceptConnection(TCPSocket *s);
@@ -25,6 +26,7 @@ private:
   bool isServerSocketFD(int);
   void handleResponse(Client *);
   void removeClient(int keyFD);
+  TCPSocket *getSocketByFD(int targetFD) const;
 
 public:
   Server(std::vector<TCPSocket *> sockets);
--- src/Server.cpp
+++ src/Server.cpp
@@ -34,45 +34,66 @@ Server::Server(std::vector<TCPSocket *> s) : _sockets(s), _clients() {
 
 // Destructor for Server class
 Server::~Server() {}
-
+TCPSocket *Server::getSocketByFD(int targetFD) const {
+  for (size_t i = 0; i < _sockets.size(); i++) {
+    if (_sockets[i]->getSocketFD() == targetFD)
+      return _sockets[i];
+  }
+  return NULL;
+}
 // Accept a new client connection
 void Server::acceptConnection(TCPSocket *s) {
-  int newClient = accept(s->getSocketFD(), (sockaddr *)&s->getSocketAdress(),
-                         &s->getSocketAddressLength());
-  if (newClient < 0) {
-    std::ostringstream ss;
-    ss << "Server failed to accept incoming connection from ADDRESS: "
-       << inet_ntoa(s->getSocketAdress().sin_addr)
-       << "; PORT: " << ntohs(s->getSocketAdress().sin_port);
-    exitWithFailure(ss.str());
-  }
-  if (fcntl(newClient, F_SETFL, O_NONBLOCK) < 0)
-    exitWithFailure("Failed to set non-blocking mode for client socket");
-  Client *n = new Client(newClient, READING);
-  _clients.insert(std::make_pair(newClient, n));
+	  int newClient = accept(s->getSocketFD(), (sockaddr *)&s->getSocketAdress(),
+				 &s->getSocketAddressLength());
+	  if (newClient < 0) {
+	    std::ostringstream ss;
+	    ss << "Server failed to accept incoming connection from ADDRESS: "
+	       << inet_ntoa(s->getSocketAdress().sin_addr)
+	       << "; PORT: " << ntohs(s->getSocketAdress().sin_port);
+	    exitWithFailure(ss.str());
+	  }
+	  if (fcntl(newClient, F_SETFL, O_NONBLOCK) < 0)
+	    exitWithFailure("Failed to set non-blocking mode for client socket");
+	  Client *n = new Client(newClient, READING);
+	  _clients.insert(std::make_pair(newClient, n));
+  
+	struct  epoll_event ev;
+	ev.data.fd = newClient;
+	ev.events = EPOLLIN;
+	if (epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, newClient, &ev) == -1) {
+		exitWithFailure("epoll ctl problem");
+	}
 }
 
 // Initialize server sockets to listen to clients
 void Server::startToListenClients()
 {
-  // Iterate over all the server sockets
-  for (size_t i = 0; i < _sockets.size(); i++)
-  {
-    // Initialize the socket
-    _sockets[i]->initSocket();
+	// Iterate over all the server sockets
+	for (size_t i = 0; i < _sockets.size(); i++)
+	{
+	// Initialize the socket
+	_sockets[i]->initSocket();
 
-    // Set the socket to listen for incoming connections
-    if (listen(_sockets[i]->getSocketFD(), 0) < 0)
-      exitWithFailure("Socket listen failed");
+	// Set the socket to listen for incoming connections
+	if (listen(_sockets[i]->getSocketFD(), 0) < 0)
+	exitWithFailure("Socket listen failed");
 
-    // Log the address and port the server is listening on
-    std::ostringstream ss;
-    ss << "\n*** Listening on ADDRESS: "
-       << inet_ntoa(_sockets[i]->getSocketAdress().sin_addr) // Convert the IP address to a string
-       << " PORT: " << ntohs(_sockets[i]->getSocketAdress().sin_port) // Convert the port number to host byte order
-       << " ***\n";
-    log(ss.str());
-  }
+	// Log the address and port the server is listening on
+	std::ostringstream ss;
+	ss << "\n*** Listening on ADDRESS: "
+	<< inet_ntoa(_sockets[i]->getSocketAdress().sin_addr) // Convert the IP address to a string
+	<< " PORT: " << ntohs(_sockets[i]->getSocketAdress().sin_port) // Convert the port number to host byte order
+	<< " ***\n";
+	log(ss.str());
+
+
+	struct  epoll_event ev;
+	ev.data.fd = _sockets[i]->getSocketFD();
+	ev.events = EPOLLIN;
+	if (epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, _sockets[i]->getSocketFD(), &ev) == -1) {
+		exitWithFailure("epoll ctl problem");
+	}
+	}
 }
 
 
@@ -96,103 +117,87 @@ bool Server::isServerSocketFD(int fd) {
 // Main loop to manage all sockets
 void Server::runServers(void)
 {
-  int byteReceived;
+	int byteReceived;
+	int MAX_EVENT = 200;
+	struct epoll_event events[MAX_EVENT];
 
-  // Initialize server sockets to listen for client connections
-  startToListenClients();
+	_epoll_fd = epoll_create1(0); 
+	if (_epoll_fd == -1)
+	        exitWithFailure("Error with Epoll_fd");
 
-  std::vector<pollfd> pollfds;
+	// Initialize server sockets to listen for client connections
+	startToListenClients();
 
-  while (true)
-  {
-    if (stopListening)
-      break;
+	while (true)
+	{
+	  if (stopListening)
+	    break;
 
-    // Clear the pollfds vector for the new polling cycle
-    pollfds.clear();
+	  log("Waiting !");
+	  // Poll the sockets to check for events
+	  int nfds = epoll_wait(_epoll_fd, events, MAX_EVENT, -1);
+	  if (stopListening)
+	    break;
+	  if (nfds == -1)
+	        exitWithFailure("Error with epoll_wait");
+	  log("New events !");
 
-    // Add server sockets to the pollfds vector
-    for (size_t i = 0; i < _sockets.size(); i++) {
-      struct pollfd pfd = {.fd = _sockets[i]->getSocketFD(), .events = POLLIN};
-      pollfds.push_back(pfd);
-    }
+	  // Iterate over pollfds to handle events
+	  for (int i = 0; i < nfds; i++) {
+		  int fd_triggered = events[i].data.fd;
+		  std::cout << "Fd: " << fd_triggered << " for " << events[i].events << std::endl;
+		  if (events[i].events & EPOLLIN)
+		  {
+			  if (_clients.count(fd_triggered))
+			  {
+			  	Client *client = _clients.at(fd_triggered);
+				byteReceived = client->readRequest();
+				if (byteReceived > 0)
+				{
+					events[i].events = EPOLLOUT;
+					if (epoll_ctl(_epoll_fd, EPOLL_CTL_MOD, fd_triggered, &events[i]) == -1) {
+						exitWithFailure("epoll ctl_mod problem");
+					}
+					 
+				} else if (byteReceived == 0){
+					this->removeClient(fd_triggered);
+				} else if (byteReceived < 0 && errno != EWOULDBLOCK && errno != EAGAIN) {
+					log(strerror(errno));
+					log("Failed to read bytes from client socket connection");
+					this->removeClient(fd_triggered);
+				}
 
-    // Add client sockets to the pollfds vector
-    std::map<unsigned short, Client *>::const_iterator it;
-    for (it = _clients.begin(); it != _clients.end(); it++)
-    {
-      const short ev = it->second->getWaitingStatus() == READING ? POLLIN : POLLOUT;
-      struct pollfd cpfd = {.fd = it->first, .events = ev};
-      pollfds.push_back(cpfd);
-    }
+			  } else {
+				  TCPSocket *server = getSocketByFD(fd_triggered);
+				  std::cout << "server: " << server->getSocketFD() << std::endl;
+				  acceptConnection(server);
+			  }
+		  } else if (_clients.count(fd_triggered) && events[i].events & EPOLLOUT) {
+			  Client *client = _clients.at(fd_triggered);
+			  handleResponse(client);
+		  } else 
+			  log("Unexpected log !");
+	  }
+	}
 
-    // Poll the sockets to check for events
-    int ret = poll(pollfds.data(), pollfds.size(), -1);
-    if (stopListening)
-      break;
-    if (ret < 0)
-      exitWithFailure("Poll error");
+	// Print information about remaining clients before closing all sockets
+	std::cout << "Clients:" << std::endl;
+	std::map<unsigned short, Client *>::iterator it;
+	for (it = _clients.begin(); it != _clients.end(); it++) {
+	  std::cout << it->second->getFd() << std::endl;
+	  std::cout << it->second->getDataSent() << std::endl;
+	  std::cout << it->second->getRequest() << std::endl;
+	}
 
-    // Iterate over pollfds to handle events
-    for (size_t i = 0; i < pollfds.size(); i++) {
-      if (pollfds[i].revents & POLLIN) {  // If there's data to read
-        if (isServerSocketFD(pollfds[i].fd)) {
-          // Accept a new client connection if the event is on a server socket
-          acceptConnection(_sockets[i]);
-        } else if (_clients.count(pollfds[i].fd)) {
-          // Read request from the client if the event is on a client socket
-          Client *client = _clients.at(pollfds[i].fd);
-          if (!client)
-            exitWithFailure("Client doesn't exist !");
-          byteReceived = client->readRequest();
-          if (byteReceived == 0) {
-            // Remove the client if the connection is closed
-            this->removeClient(pollfds[i].fd);
-          } else if (byteReceived < 0 && errno != EWOULDBLOCK) {
-            log(strerror(errno));
-            log("Failed to read bytes from client socket connection");
-            this->removeClient(pollfds[i].fd);
-          }
-        }
-      } else if (pollfds[i].revents & POLLOUT) {  // If the socket is ready to write
-        Client *client = _clients.at(pollfds[i].fd);
-        if (!client)
-          exitWithFailure("Client doesn't exist !");
-        // Send response to the client
-        handleResponse(client);
-      } else if (_clients.count(pollfds[i].fd) &&
-                 (pollfds[i].revents & POLLERR ||
-                  pollfds[i].revents & POLLHUP ||
-                  pollfds[i].revents & POLLNVAL)) {
-        // Handle errors on the client socket
-        log("This fd has an error !");
-        removeClient(pollfds[i].fd);
-      } else {
-        log("Unknown");
-        std::cout << pollfds[i].fd << " -> " << pollfds[i].events << " -> "
-                  << pollfds[i].revents << std::endl;
-      }
-    }
-  }
-
-  // Print information about remaining clients before closing all sockets
-  std::cout << "Clients:" << std::endl;
-  std::map<unsigned short, Client *>::iterator it;
-  for (it = _clients.begin(); it != _clients.end(); it++) {
-    std::cout << it->second->getFd() << std::endl;
-    std::cout << it->second->getDataSent() << std::endl;
-    std::cout << it->second->getRequest() << std::endl;
-  }
-
-  // Close all server sockets
-  closeAllSockets();
+	// Close all server sockets
+	closeAllSockets();
 }
 
 
 // Handle client response
 void Server::handleResponse(Client *client) {
   client->sendResponse();
-  if (client->getDataSent() == 0) {
+  if (client->getDataSent() <= 0) {
     removeClient(client->getFd());
   }
 }
@@ -208,6 +213,10 @@ void Server::removeClient(int keyFD) {
   std::map<unsigned short, Client *>::iterator element = _clients.find(keyFD);
   if (element == _clients.end())
     return;
+  // TODO REMOVE CLIENT FROM EPOLL EVENT
+  if (epoll_ctl(_epoll_fd, EPOLL_CTL_DEL, keyFD, NULL) == -1) {
+	  exitWithFailure("epoll ctl_del problem");
+  }
   close(keyFD);
   _clients.erase(element);
   delete element->second;
